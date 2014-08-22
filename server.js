1be0ad485470c0a85138408f97bf7dc70f483837
Adrien Cahen <adrien@continuuity.com>
2014-08-21 18:09:35 -0700

    colors

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-var port = process.env.COOPR_UI_PORT || 8080;
+var port = process.env.COOPR_UI_PORT || 8080,
+    httpLabel = '\x1B[7m http-server \x1B[27m',
+    corsLabel = '\x1B[7m cors-anywhere \x1B[27m';
 
 require('http-server')
   .createServer({
     root: __dirname + '/dist',
     logFn: function (req) {
-      console.log('\n[http-server] %s', (new Date).toUTCString());
-      console.log('%s %s', req.method, req.url);
+      console.log(
+        '\n%s %s\n\x1B[1m%s\x1B[22m \x1B[36m%s\x1B[39m',
+        httpLabel,
+        (new Date).toUTCString(),
+        req.method,
+        req.url
+      );
     }
   })
   .listen(port, '0.0.0.0', function () {
-    console.log('[http-server] listening on port %s', port);
+    console.log(httpLabel+' listening on port %s', port);
   });
 
 
@@ -19,11 +26,18 @@ require('cors-anywhere')
     removeHeaders: ['cookie', 'cookie2']
   })
   .listen(8081, '0.0.0.0', function() {
-    console.log('[cors-anywhere] listing on port 8081');
+    console.log(corsLabel+' listening on port 8081');
   })
   .proxy.on('proxyResponse', function (res) {
-    console.log('\n[cors-anywhere] %s', (new Date).toUTCString());
-    console.log('%s %s', res.method, res.corsAnywhereRequestState.location.full_url);
+    var location = res.corsAnywhereRequestState.location;
+    console.log(
+      '\n%s %s\n\x1B[1m%s\x1B[22m %s\x1B[36m%s\x1B[39m',
+      corsLabel,
+      (new Date).toUTCString(),
+      res.method, 
+      (location.isHttps ? 'https://' : 'http://') + location.host,
+      location.pathAndQueryString || '/'
+    );
   });
 
 
